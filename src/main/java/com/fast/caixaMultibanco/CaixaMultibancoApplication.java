c89dcb5485a0343fe1bde063fbb67fafca437773
AllanBastos <allan.santos@academico.ifpb.edu.br>
2021-07-14 13:52:33 -0300

    add hello world

--- src/main/java/com/fast/caixaMultibanco/CaixaMultibancoApplication.java
+++ src/main/java/com/fast/caixaMultibanco/CaixaMultibancoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CaixaMultibancoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CaixaMultibancoApplication.class, args);
+		System.out.println("Hello Word");
 	}
 
 }
