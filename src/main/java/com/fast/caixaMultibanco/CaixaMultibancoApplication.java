296822438facedc041ab033dc0a9c5a5c469a107
AllanBastos <allan.santos@academico.ifpb.edu.br>
2021-07-14 13:37:14 -0300

    criando projeto

---
+++ src/main/java/com/fast/caixaMultibanco/CaixaMultibancoApplication.java
@@ -0,0 +1,13 @@
+package com.fast.caixaMultibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaMultibancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaMultibancoApplication.class, args);
+	}
+
+}
