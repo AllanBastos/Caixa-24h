ec82ef129e4ef4929d8bdfa2c2692ee1a47daac3
AllanBastos <allan.santos@academico.ifpb.edu.br>
2021-07-14 17:50:52 -0300

    atualizando entidades e criando nova exceção

--- src/main/java/com/fast/entidades/Caixa.java
+++ src/main/java/com/fast/entidades/Caixa.java
@@ -4,7 +4,9 @@ package com.fast.entidades;
 
 public class Caixa {
 
+	private static final int MAX_CEDULAS = 200;
 	// Atributos
+	private Integer codigo_caixa; //Código_caixa: gerado automáticamente;
 	private int qtd_cedulas_2;
 	private int qtd_cedulas_5;
 	private int qtd_cedulas_10;
@@ -12,10 +14,11 @@ public class Caixa {
 
 	// Construtor
 	public Caixa(int qtd_cedulas_2, int qtd_cedulas_5, int qtd_cedulas_10, int qtd_cedulas_50) {
-		this.qtd_cedulas_2 = qtd_cedulas_2;
-		this.qtd_cedulas_5 = qtd_cedulas_5;
-		this.qtd_cedulas_10 = qtd_cedulas_10;
-		this.qtd_cedulas_50 = qtd_cedulas_50;
+		setQtd_cedulas_2(qtd_cedulas_2);
+		setQtd_cedulas_5(qtd_cedulas_5);
+		setQtd_cedulas_10(qtd_cedulas_10);
+		setQtd_cedulas_50(qtd_cedulas_50);
+		
 	}
 
 	public int getQtd_cedulas_2() {
@@ -23,15 +26,25 @@ public class Caixa {
 	}
 
 	public void setQtd_cedulas_2(int qtd_cedulas_2) {
-		this.qtd_cedulas_2 = qtd_cedulas_2;
+		
+		if (validarQuantidade(qtd_cedulas_2)) {
+			this.qtd_cedulas_2 = qtd_cedulas_2;
+		}else {
+			throw new EntitiesException("Quantidades de cedulas invalidas");
+		}
 	}
+
 
 	public int getQtd_cedulas_5() {
 		return qtd_cedulas_5;
 	}
 
 	public void setQtd_cedulas_5(int qtd_cedulas_5) {
-		this.qtd_cedulas_5 = qtd_cedulas_5;
+		if(validarQuantidade(qtd_cedulas_5)) {
+			this.qtd_cedulas_5 = qtd_cedulas_5;
+		}else {
+			throw new EntitiesException("Quantidades de cedulas invalidas");
+		}
 	}
 
 	public int getQtd_cedulas_10() {
@@ -39,7 +52,11 @@ public class Caixa {
 	}
 
 	public void setQtd_cedulas_10(int qtd_cedulas_10) {
-		this.qtd_cedulas_10 = qtd_cedulas_10;
+		if(validarQuantidade(qtd_cedulas_10)) {
+			this.qtd_cedulas_10 = qtd_cedulas_10;
+		}else {
+			throw new EntitiesException("Quantidades de cedulas invalidas");
+		}
 	}
 
 	public int getQtd_cedulas_50() {
@@ -47,6 +64,18 @@ public class Caixa {
 	}
 
 	public void setQtd_cedulas_50(int qtd_cedulas_50) {
-		this.qtd_cedulas_50 = qtd_cedulas_50;
+		if(validarQuantidade(qtd_cedulas_50)) {
+			this.qtd_cedulas_5 = qtd_cedulas_50;
+		}else {
+			throw new EntitiesException("Quantidades de cedulas invalidas");
+		}	
 	}
+	
+	private boolean validarQuantidade(int qtd_cedulas) {
+		if ( qtd_cedulas <= MAX_CEDULAS & qtd_cedulas >= 0) {			
+			return true;
+		}
+		return false;
+	}
+	
 }
---
+++ src/main/java/com/fast/entidades/EntitiesException.java
@@ -0,0 +1,11 @@
+package com.fast.entidades;
+
+public class EntitiesException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public EntitiesException(String msg) {
+		super(msg);
+	}
+	
+	
+}
