4d3393b298256f84e2e51c5ec52356c940d0ed4d
AllanBastos <allan.santos@academico.ifpb.edu.br>
2021-07-21 17:52:58 -0300

    Deploy app to Heroku

--- src/main/java/com/fast/caixaMultibanco/CarregarBaseDados.java
+++ src/main/java/com/fast/caixaMultibanco/CarregarBaseDados.java
@@ -32,23 +32,35 @@ public class CarregarBaseDados {
 			/*
 			 * Inserindo Bancos
 			 */
-			String path =  ".\\src\\main\\resources\\Bancos.csv";
-			try (BufferedReader br = new BufferedReader(new FileReader(path))){
-				String line = br.readLine();
-				Integer ispb = 0;
-				while(line != null) {
-					String[] fields = line.split(",");
-					ispb = Integer.parseInt((fields[1].toString().trim()));
-					int codigo = Integer.parseInt(fields[3]);
-					log.info("Carregando " + banco.save(new Banco(ispb, fields[2], codigo, fields[4], fields[5], fields[6])));
-					line = br.readLine();
-				}
-			}catch(IOException e) {
-				System.out.println("Error: " + e.getMessage());
-			}catch(NumberFormatException e) {
-				System.out.println(e.getMessage());
-			}
+//			String path =  ".\\src\\main\\resources\\Bancos.csv";
+//			try (BufferedReader br = new BufferedReader(new FileReader(path))){
+//				String line = br.readLine();
+//				Integer ispb = 0;
+//				while(line != null) {
+//					String[] fields = line.split(",");
+//					ispb = Integer.parseInt((fields[1].toString().trim()));
+//					int codigo = Integer.parseInt(fields[3]);
+//					log.info("Carregando " + banco.save(new Banco(ispb, fields[2], codigo, fields[4], fields[5], fields[6])));
+//					line = br.readLine();
+//				}
+//			}catch(IOException e) {
+//				System.out.println("Error: " + e.getMessage());
+//			}catch(NumberFormatException e) {
+//				System.out.println(e.getMessage());
+//			}
 			
+			log.info("Carregando " + banco.save(new Banco(00000000,"BCO DO BRASIL S.A.",001,"Sim","RSFN","Banco do Brasil S.A.")));
+			log.info("Carregando " + banco.save(new Banco(208,"BRB - BCO DE BRASILIA S.A.",070,"Sim","RSFN","BRB - BANCO DE BRASILIA S.A.")));
+			log.info("Carregando " + banco.save(new Banco(00360305,"CAIXA ECONOMICA FEDERAL",104,"Sim","RSFN","CAIXA ECONOMICA FEDERAL")));
+			log.info("Carregando " + banco.save(new Banco(4902979,"BCO DA AMAZONIA S.A.",003,"Sim","RSFN","BANCO DA AMAZONIA S.A.")));
+			log.info("Carregando " + banco.save(new Banco(10573521,"MERCADO PAGO" ,323,"Não","RSFN","MERCADOPAGO.COM REPRESENTACOES LTDA.")));
+			log.info("Carregando " + banco.save(new Banco(13009717,"BCO DO EST. DE SE S.A.",047,"Sim","RSFN","Banco do Estado de Sergipe S.A.")));
+			log.info("Carregando " + banco.save(new Banco(18236120,"NU PAGAMENTOS S.A.",260,"Não","RSFN","Nu Pagamentos S.A.")));
+			log.info("Carregando " + banco.save(new Banco(22896431,"PICPAY" ,380,"Não","RSFN","PICPAY SERVICOS S.A." )));
+			log.info("Carregando " + banco.save(new Banco(60701190,"ITAÚ UNIBANCO S.A.",341,"Sim","RSFN","ITAÚ UNIBANCO S.A.")));
+			log.info("Carregando " + banco.save(new Banco(60746948,"BCO BRADESCO S.A.",237,"Sim","RSFN","Banco Bradesco S.A.")));
+			log.info("Carregando " + banco.save(new Banco(33657248,"BNDES", 007 ,"Não"	,"RSFN"	,"BANCO NACIONAL DE DESENVOLVIMENTO ECONOMICO E SOCIAL")));
+	
 			/*
 			 * Inserindo Caixas
 			 */
