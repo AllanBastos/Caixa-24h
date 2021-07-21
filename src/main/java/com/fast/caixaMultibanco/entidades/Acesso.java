d92c5e517dc571e2c7360175bac900c06d16e81c
AllanBastos <allan.santos@academico.ifpb.edu.br>
2021-07-21 15:08:49 -0300

    adicionado 15 minutos ao tempo de acesso

--- src/main/java/com/fast/caixaMultibanco/entidades/Acesso.java
+++ src/main/java/com/fast/caixaMultibanco/entidades/Acesso.java
@@ -130,7 +130,7 @@ public class Acesso {
 	 * @param tempoFinal the tempoFinal to set
 	 */
 	public void setTempoFinal() {
-		this.tempoFinal = getTempoInicial() + 60000; //900000 Representa 15 minutos
+		this.tempoFinal = getTempoInicial() + 900000; //900000 Representa 15 minutos
 	}
 
 	/**
